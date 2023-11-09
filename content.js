3e40163d5b1f740fb60ea818d8f43c9e189f06e4
josquindesprez <eaghassemi@gmail.com>
2023-11-09 02:52:28 +0100

    added sort by number function

--- content.js
+++ content.js
@@ -24,25 +24,7 @@ document.addEventListener("DOMContentLoaded", function () {
     document.body.style.backgroundImage = `url('${imageUrl}')`;
 });
 
-function addProgressiveNumbersToTasks() {
-  // Retrieve the taskLists object from local storage
-  let taskLists = JSON.parse(localStorage.getItem('taskLists'));
 
-  if (taskLists) {
-    let currentNumber = 1; // Start numbering from 1
-    taskLists.forEach(list => {
-      list.tasks.forEach(task => {
-        task.number = currentNumber++; // Assign the current number and increment it
-      });
-    });
-
-    // Save the updated task lists back to local storage
-    localStorage.setItem('taskLists', JSON.stringify(taskLists));
-    console.log('Task numbers updated successfully!');
-  } else {
-    console.log('No taskLists found in local storage.');
-  }
-}
 
 // Example usage
 addProgressiveNumbersToTasks();
@@ -484,7 +466,9 @@ function setScrollbarColors(thumbColor, trackColor) {
   `;
 }
 
-
+function sortByNumberAttribute(arr) {
+  return arr.sort((a, b) => a.number - b.number);
+}
 function renderSelectedList(taskarray) {
   const wordClock = document.getElementById('clock');
   wordClock.style.opacity = 0;
@@ -492,6 +476,7 @@ function renderSelectedList(taskarray) {
 
   makeClockHorizontal(); 
   const lista = taskarray;
+  console.log(sortByNumberAttribute(lista))
   console.log(lista)
   const listColumn = document.getElementById('listColumn')
   const listContainer = document.getElementById('todoSelSubsection');
